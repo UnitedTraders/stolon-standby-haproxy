03352d8bceee6e3e00f259fb76048a0a7755f96d
Anton Markelov <doublic@gmail.com>
2018-08-14 17:07:49 +1000

    ADMIN-1709 fix file overwriting

--- src/main.py
+++ src/main.py
@@ -21,29 +21,33 @@ if __name__ == '__main__':
 
     while True:
 
-        stolon_json = json.loads(check_output("stolonctl clusterdata", shell=True))
+        stolon_json = json.loads(check_output(
+            "stolonctl clusterdata", shell=True))
         haproxy_template = open('./stolon_haproxy.j2', 'r')
 
         standby_list = []
 
-        # get standby's 
+        # get standby's
         for db in stolon_json['DBs']:
             if stolon_json['DBs'][db]['status']['healthy'] and stolon_json['DBs'][db]['spec']['role'] == 'standby':
-                standby_list.append(stolon_json['DBs'][db]['status']['listenAddress'] + ':' + stolon_json['DBs'][db]['status']['port'])
+                standby_list.append(
+                    stolon_json['DBs'][db]['status']['listenAddress'] + ':' + stolon_json['DBs'][db]['status']['port'])
         # print(standby_list)
 
         template = Template(haproxy_template.read())
         new_render = template.render(servers=standby_list,
-                                frontend_port=config['postgres_haproxy_port'])
+                                     frontend_port=config['postgres_haproxy_port'])
 
-        haproxy_config = open(config['postgres_haproxy_config'], 'w+')
+        haproxy_config = open(config['postgres_haproxy_config'], 'r')
         if haproxy_config.read() == new_render:
             print("Config not changed!")
         else:
             print("Config changed!")
+            haproxy_config.close()
+            haproxy_config = open(config['postgres_haproxy_config'], 'w')
             haproxy_config.write(new_render)
             run(config['haproxy_reload_command'], shell=True, check=True)
-            
+
         haproxy_config.close()
         haproxy_template.close()
 
